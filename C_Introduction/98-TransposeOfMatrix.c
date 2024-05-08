4fe653a3225675fec17d37fb629357a4bbc8dfa1
aaryakumbhar4 <158166369+aaryakumbhar4@users.noreply.github.com>
2024-05-08 19:22:24 +0000

    98-TransposeOfMatrix.c

---
+++ C_Introduction/98-TransposeOfMatrix.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &m);
+    printf("Enter the number of columns: ");
+    scanf("%d", &n);
+
+    int matrix[m][n], transpose[n][m];
+
+    printf("Enter the elements of the matrix:\n");
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("The original matrix is:\n");
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+
+    printf("The transpose of the matrix is:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
