67599f85874643dc729ea8cb3ccd25a97347555c
aaryakumbhar4 <158166369+aaryakumbhar4@users.noreply.github.com>
2024-05-09 20:12:01 +0000

    100-MatrixSubtraction.c

---
+++ C_Introduction/100-MatrixSubtraction.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols, i, j;
+
+    printf("Enter the number of rows and columns of the matrices: ");
+    scanf("%d%d", &rows, &cols);
+
+    int matrix1[rows][cols], matrix2[rows][cols], difference[rows][cols];
+
+    printf("Enter the elements of the first matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+
+    printf("Enter the elements of the second matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+
+    printf("\nThe first matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%d\t", matrix1[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nThe second matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%d\t", matrix2[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            difference[i][j] = matrix1[i][j] - matrix2[i][j];
+        }
+    }
+
+    printf("\nThe difference of the matrices:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%d\t", difference[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
